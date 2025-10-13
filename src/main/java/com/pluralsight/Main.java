d3ed60a3c48987e1d5f3f95f3c6f3a2e86c86ae6
kai elijah gronholz <kaielijah2113@gmail.com>
2025-10-13 14:54:01 -0500

    Added a while loop and switch that accounts for the diffrent possible inputs from the user andcycles the home screen.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,10 @@
 //import files
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         homeScreen();
 
@@ -9,11 +12,50 @@ public class Main {
 
     //create home screen
     private static void homeScreen(){
-        System.out.println("\n----Finance Tracker----\n");
-        System.out.println("Please enter a character to make your selection.");
-        System.out.println("D) Add Deposit");
-        System.out.println("P) Make Payment");
-        System.out.println("L) Ledger");
-        System.out.println("X) Exit");
+        //Loop to repeat promt and cycle switch
+        while (true) {
+
+            System.out.println("\n----Finance Tracker----\n");
+            System.out.println("Please enter a character to make your selection.");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+
+            //scanner to take input from user
+            String choice = scanner.nextLine().trim().toUpperCase();
+
+            //switch for interpreting the users choice.
+            switch (choice){
+                case "D": makeDeposit();
+                    break;
+                case "P": makePayment();
+                    break;
+                case "L": displayLedger();
+                    break;
+                case "X":
+                    System.out.println("Thank you for using my fanance tracker!");
+                    return;
+
+                default:
+                    System.out.println("Invalid option. Try again.");
+            }
+        }
     }
+
+    //function to make deposits
+    private static void makeDeposit(){
+        System.out.println("Make a Deposit");
+    }
+    //function to make payments
+    private static void makePayment(){
+        System.out.println("Make a payment");
+    }
+    //function for ledger
+    private static void displayLedger(){
+        System.out.println("Ledger");
+
+    }
+
+
 }
