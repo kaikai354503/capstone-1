83a8e658afa641607876f5ea0af920cec302bf96
kai elijah gronholz <kaielijah2113@gmail.com>
2025-10-13 13:41:04 -0500

    Added a display for the home screen function.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,19 @@
+//import files
 package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        homeScreen();
 
+    }
+
+    //create home screen
+    private static void homeScreen(){
+        System.out.println("\n----Finance Tracker----\n");
+        System.out.println("Please enter a character to make your selection.");
+        System.out.println("D) Add Deposit");
+        System.out.println("P) Make Payment");
+        System.out.println("L) Ledger");
+        System.out.println("X) Exit");
     }
 }
